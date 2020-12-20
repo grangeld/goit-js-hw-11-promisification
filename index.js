54b77d654b1aeed0e5ffa51b040caef6fea3d37e
Dima <grangeld3@gmail.com>
2020-12-20 23:18:14 +0200

    change error

--- index.js
+++ index.js
@@ -52,7 +52,7 @@ const makeTransaction = (transaction) => {
       const canProcess = Math.random() > 0.3;
 
       if (canProcess) {
-        resolve(transaction.id, delay);
+        resolve([transaction.id, delay]);
       } else {
         reject(transaction.id);
       }
@@ -60,7 +60,7 @@ const makeTransaction = (transaction) => {
   });
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ([id, time]) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
